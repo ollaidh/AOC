cbd2aaa7d64fb99514080e2f3499ab8ff656dc05
ollaidh <ollaidh@programmer.net>
2024-10-01 16:19:12 +0300

    2022. Day 2. Part 2.

--- 2022/test_day_2.py
+++ 2022/test_day_2.py
@@ -12,19 +12,11 @@ figures_relations = {
     "C": {"X": 6, "Y": 0, "Z": 3},
 }
 
+required_results = {
+    "X": 0, "Y": 3, "Z": 6
+}
 
-def play_round(my_move: int, elf_move: int) -> int:
-    return figures_relations[elf_move][my_move] + figures_weights[my_move]
-
-
-def play(moves: list[list[str]]) -> int:
-    result = 0
-    for move in moves:
-        result  += play_round(move[1], move[0])
-    return result
-
-
-def get_moves_from_file(filepath: str) -> list[list[str]]:
+def get_data_from_file(filepath: str) -> list[list[str]]:
     file = pathlib.Path(__file__).parent.resolve() / filepath
     moves = []
     with open(file) as f:
@@ -34,6 +26,24 @@ def get_moves_from_file(filepath: str) -> list[list[str]]:
             moves.append(move)
     return moves
 
+def define_my_moves(plan: list[list[str]]):
+    for i in range(len(plan)):
+        elf_move = plan[i][0]
+        required_result = plan[i][1]
+        for key, value in figures_relations[elf_move].items():
+            if value == required_results[required_result]:
+                plan[i][1] = key
+    return plan
+
+def play_round(my_move: str, elf_move: str) -> int:
+    return figures_relations[elf_move][my_move] + figures_weights[my_move]
+
+
+def play(moves: list[list[str]]) -> int:
+    result = 0
+    for move in moves:
+        result  += play_round(move[1], move[0])
+    return result
 
 def test_play():
     moves = [
@@ -46,6 +56,10 @@ def test_play():
 
 
 if __name__ == "__main__":
-   moves = get_moves_from_file("inputs/input_day_2.txt")
-   print(play(moves))
+   data = get_data_from_file("inputs/input_day_2.txt")
+   first_part_result = play(data)
+   moves = define_my_moves(data)
+   second_part_result = play(moves)
+   print(second_part_result)
+
 
