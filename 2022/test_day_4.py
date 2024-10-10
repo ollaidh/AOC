32afd9948a2aec7b07bc0d91be2ebd892b69f0f1
ollaidh <ollaidh@programmer.net>
2024-10-10 20:53:58 +0300

    2022. Day 4. Part 2

--- 2022/test_day_4.py
+++ 2022/test_day_4.py
@@ -20,10 +20,25 @@ def count_full_overlaps(pairs: list[list[list[int]]]) -> int:
         if (pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]) or (pair[0][0] <= pair[1][0] and pair[0][1] >= pair[1][1]):
                 counter += 1
     return counter
-          
 
+def count_overlaps(pairs: list[list[list[int]]]) -> int:
+    counter = 0
+    for pair in pairs:
+        begin_1 = pair[0][0]
+        end_1 = pair[0][1]
+        begin_2 = pair[1][0]
+        end_2 = pair[1][1]
+
+        if  begin_1 <= begin_2 <= end_1 or begin_2 <= begin_1 <= end_2:
+             counter += 1
+
+    return counter
+
+# (3, 8) (1, 4)
+          
 
 if __name__ == "__main__":
         filepath = pathlib.Path(__file__).parent.resolve() / "inputs/input_day_4.txt"
         pairs = read_pairs(filepath)
-        print(count_full_overlaps(pairs))
+        result_part_1 = count_full_overlaps(pairs)
+        result_part_2 = count_overlaps(pairs)
