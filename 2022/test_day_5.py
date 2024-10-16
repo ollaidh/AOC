022e93103aecd33629f6d1a2cc22b6c3e752ca9d
ollaidh <ollaidh@programmer.net>
2024-10-16 21:36:23 +0300

    Day 5. Part 1. Get moves plan func

--- 2022/test_day_5.py
+++ 2022/test_day_5.py
@@ -4,9 +4,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Move:
-    amount: int
-    move_from: int
-    move_to: int 
+    def __init__(self, amount: int, move_from: int, move_to: int):
+        self.amount = amount
+        self.move_from = move_from
+        self.move_to = move_to
+    
+    def __eq__(self, value: object) -> bool:
+        return self.amount == value.amount and self.move_from == value.move_from and self.move_to == value.move_to
 
 
 def get_stacks_plan(lines: list[str]) -> list[list[str]]:
@@ -19,6 +23,15 @@ def get_stacks_plan(lines: list[str]) -> list[list[str]]:
         if curr_stack:
             stacks.append(curr_stack)
     return stacks
+
+
+def get_moves_plan(lines: list[str]) -> list[Move]:
+    moves = []
+    for line in lines:
+        elements = line.split()
+        move = Move(int(elements[1]), int(elements[3]), int(elements[5]))
+        moves.append(move)
+    return moves
 
 
 def test_get_stacks():
@@ -34,8 +47,23 @@ def test_get_stacks():
         ["P"]
     ]
     stacks = get_stacks_plan(data)
-    print(expected)
-    print(stacks)
     assert stacks == expected
+
+
+def test_get_plan():
+    data = [
+        "move 1 from 2 to 1",
+        "move 3 from 1 to 3",
+        "move 2 from 25 to 1",
+        "move 1 from 1 to 2"
+    ]
+    expected_result = [
+        Move(1, 2, 1),
+        Move(3, 1, 3),
+        Move(2, 25, 1),
+        Move(1, 1, 2)
+    ]
+    plan = get_moves_plan(data)
+    assert plan == expected_result
 
 
