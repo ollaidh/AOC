7b887b6f76fbd856101faa6dedb661403b937a7f
ollaidh <ollaidh@programmer.net>
2024-12-04 21:20:02 +0200

    2024. Day 3. Part 1. Refactor

--- 2024/test_day_3.py
+++ 2024/test_day_3.py
@@ -7,16 +7,16 @@ def get_muls(input: str):
     return matches
 
 
-def calc_muls_result(input: str) -> dict:
-    result: dict = {}
-    result["result"] = 0
-    result["mults_results"] = []
+def calc_muls(input: str) -> list[int]:
+    result = []
     muls = get_muls(input)
     for mul in muls:
         curr = int(mul[0]) * int(mul[1])
-        result["result"] += curr
-        result["mults_results"].append(curr)
+        result.append(curr)
     return result
+
+def sum_muls(muls: list[int]) -> int:
+    return sum(muls)
 
 
 def test_get_muls():
@@ -27,14 +27,16 @@ def test_get_muls():
 
 def test_calc_muls_result():
     input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-    result = calc_muls_result(input)
-    assert result["result"] == 161
-    assert result["mults_results"] == [8, 25, 88, 40]
+    muls = calc_muls(input)
+    assert muls == [8, 25, 88, 40]
+    result = sum_muls(muls)
+    assert result == 161
 
 
 if __name__ == "__main__":
     filepath = Path(__file__).parent.resolve() / "inputs/input_day_3.txt"
     with open(filepath) as f:
         input = f.read()
-    result_part_1 = calc_muls_result(input)["result"]
+    muls = calc_muls(input)
+    result_part_1 = sum_muls(muls)
     print(result_part_1)
